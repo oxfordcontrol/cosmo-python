0b7f6684905e6af9ae1b3573893e4c1919595f81
Michael Garstka <michael@garstka.org>
2020-10-27 21:31:45 +0000

    Add tox unit testing and travis script

---
+++ cosmopy/install.jl
@@ -0,0 +1,5 @@
+using Pkg
+Pkg.add("COSMO")
+Pkg.add("PyCall")
+using COSMO
+using PyCall
--- setup.py
+++ setup.py
@@ -5,21 +5,17 @@ this_directory = path.abspath(path.dirname(__file__))
 with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
-with open('requirements.txt') as f:
-    requirements = f.read().splitlines()
-
 setuptools.setup(
     name="cosmopy",
-    version="0.0.1",
+    version="0.0.2",
     author="Michael Garstka",
     author_email="michael@garstka.org",
     description="An interface to the Julia solver COSMO.jl.",
     url="https://github.com/oxfordcontrol/cosmopy",
-    package_dir={ 'cosmopy' : 'cosmopy'},
-    setup_requires=["numpy >= 1.7", "scipy >= 0.13.2", "julia >= 0.5.6"],
-    install_requires=requirements,
+    install_requires=["numpy >= 1.7", "scipy >= 0.13.2", "julia >= 0.5.6"],
     long_description=long_description,
     long_description_content_type='text/markdown',
+    packages= setuptools.find_packages(),
     python_requires='>=3.6',
     license='Apache 2.0'
 )
